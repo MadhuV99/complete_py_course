7f9b29707372ab2f5a5c73c179ce803ee29b6d5e
Madhu Vasudevan <madhuvasudevan@yahoo.com>
2021-01-27 12:48:50 -0600

    Latest Changes

--- browser_automation/app.py
+++ browser_automation/app.py
@@ -1,19 +1,34 @@
 from selenium import webdriver
 
-from pages.quotes_page import QuotesPage
+from pages.quotes_page import QuotesPage, InvalidTagForAuthorError
 
-chrome = webdriver.Chrome(executable_path="/max/myApplix/chromedr/chromedriver.exe")
-chrome.get('http://quotes.toscrape.com/search.aspx')
-page = QuotesPage(chrome)
+# chrome = webdriver.Chrome(executable_path="/max/myApplix/chromedr/chromedriver.exe")
+# chrome.get('http://quotes.toscrape.com/search.aspx')
+# page = QuotesPage(chrome)
+#
+# author = input("Enter the author you'd like quotes from: ")
+# page.select_author(author)
+#
+# tags = page.get_available_tags()
+# print("Select one of these tags: [{}]".format(" | ".join(tags)))
+# selected_tag = input("Enter your tag: ")
+#
+# page.select_tag(selected_tag)
+# page.search_button.click()
+#
+# print(page.quotes)
 
-author = input("Enter the author you'd like quotes from: ")
-page.select_author(author)
+try:
+    author = input("Enter the author you'd like quotes from: ")
+    tag = input("Enter your tag: ")
 
-tags = page.get_available_tags()
-print("Select one of these tags: [{}]".format(" | ".join(tags)))
-selected_tag = input("Enter your tag: ")
+    chrome = webdriver.Chrome(executable_path="/max/myApplix/chromedr/chromedriver.exe")
+    chrome.get("http://quotes.toscrape.com/search.aspx")
+    page = QuotesPage(chrome)
 
-page.select_tag(selected_tag)
-page.search_button.click()
-
-print(page.quotes)
+    print(page.search_for_quotes(author, tag))
+except InvalidTagForAuthorError as e:
+    print(e)
+except Exception as e:
+    print(e)
+    print("An unknown error occurred. Please try again.")
--- browser_automation/pages/quotes_page.py
+++ browser_automation/pages/quotes_page.py
@@ -1,5 +1,10 @@
 from typing import List
+
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.wait import WebDriverWait
 
 from locators.quotes_page_locators import QuotesPageLocators
 from parsers.quote import QuoteParser
@@ -47,3 +52,25 @@ class QuotesPage:
     def select_tag(self, tag_name: str):
         self.tags_dropdown.select_by_visible_text(tag_name)
 
+    def search_for_quotes(self, author_name: str, tag_name: str) -> List[QuoteParser]:
+        self.select_author(author_name)
+
+        # https://selenium-python.readthedocs.io/waits.html
+        # WebDriverWait(self.browser, 10).until(
+        #     expected_conditions.presence_of_element_located(
+        #         (By.CSS_SELECTOR, QuotesPageLocators.TAG_DROPDOWN_VALUE_OPTION)
+        #     )
+        # )
+
+        try:
+            self.select_tag(tag_name)
+        except NoSuchElementException:
+            raise InvalidTagForAuthorError(
+                f"Author '{author_name}' does not have any quotes tagged with '{tag_name}'."
+            )
+        self.search_button.click()
+        return self.quotes
+
+
+class InvalidTagForAuthorError(ValueError):
+    pass
